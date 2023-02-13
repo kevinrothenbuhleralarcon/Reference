e61fd8fa6714b5a9cd3e6d3c58f14a2e32b38ce3
Kevin Rothenbühler-Alarcon <kevin.rothenbuhler@gmail.com>
2023-02-13 20:13:17 +0100

    Finish adding all the mapping for the grades

--- Spring/JPA-Database-communication/src/main/java/com/ltp/gradesubmission/entity/Grade.java
+++ Spring/JPA-Database-communication/src/main/java/com/ltp/gradesubmission/entity/Grade.java
@@ -1,24 +1,12 @@
 package com.ltp.gradesubmission.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class Grade {
 
     private Long id;
     private String score;
-
-    public Long getId() {
-        return this.id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getScore() {
-        return this.score;
-    }
-
-    public void setScore(String score) {
-        this.score = score;
-    }
-
 }
--- Spring/JPA-Database-communication/src/main/java/com/ltp/gradesubmission/web/GradeController.java
+++ Spring/JPA-Database-communication/src/main/java/com/ltp/gradesubmission/web/GradeController.java
@@ -3,21 +3,61 @@ package com.ltp.gradesubmission.web;
 import com.ltp.gradesubmission.entity.Grade;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
 @RequestMapping("/grade")
 public class GradeController {
-    
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Grade>> getGrades() {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping("/student/{studentId}")
+    public ResponseEntity<List<Grade>> getStudentGrades(@PathVariable final Long studentId) {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping("/course/{courseId}")
+    public ResponseEntity<List<Grade>> getCourseGrades(@PathVariable final Long courseId) {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
     @GetMapping("/student/{studentId}/course/{courseId}")
     public ResponseEntity<Grade> getGrade(
             @PathVariable final Long studentId,
             @PathVariable final Long courseId
-            ) {
+    ) {
         return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PostMapping("/student/{studentId}/course/{courseId}")
+    public ResponseEntity<Grade> saveGrade(
+            @PathVariable final Long studentId,
+            @PathVariable final Long courseId,
+            @RequestBody final Grade grade
+    ) {
+        return new ResponseEntity<>(grade, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/student/{studentId}/course/{courseId}")
+    public ResponseEntity<Grade> updateGrade(
+            @PathVariable final Long studentId,
+            @PathVariable final Long courseId,
+            @RequestBody final Grade grade
+    ) {
+        return new ResponseEntity<>(grade, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/student/{studentId}/course/{courseId}")
+    public ResponseEntity<HttpStatus> deleteGrade(
+            @PathVariable final Long studentId,
+            @PathVariable final Long courseId
+    ) {
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
