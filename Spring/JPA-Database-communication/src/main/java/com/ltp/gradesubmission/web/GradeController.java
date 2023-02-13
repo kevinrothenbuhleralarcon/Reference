cb4f65fbae27f18eb843c10a08bf380a6d76c427
Kevin Rothenbühler-Alarcon <kevin.rothenbuhler@gmail.com>
2023-02-13 19:10:19 +0100

    Start adding mapping for grade

--- Spring/JPA-Database-communication/src/main/java/com/ltp/gradesubmission/web/GradeController.java
+++ Spring/JPA-Database-communication/src/main/java/com/ltp/gradesubmission/web/GradeController.java
@@ -1,5 +1,10 @@
 package com.ltp.gradesubmission.web;
 
+import com.ltp.gradesubmission.entity.Grade;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/grade")
 public class GradeController {
     
-
+    @GetMapping("/student/{studentId}/course/{courseId}")
+    public ResponseEntity<Grade> getGrade(
+            @PathVariable final Long studentId,
+            @PathVariable final Long courseId
+            ) {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
