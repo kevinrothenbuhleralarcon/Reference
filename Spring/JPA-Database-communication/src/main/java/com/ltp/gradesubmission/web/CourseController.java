4d99b365ac25cf8bd908652460319ace5e58b860
KevinRothenbühler-Al <kevin.rothenbuhler-alarcon@softcom.pro>
2023-02-14 07:27:35 +0100

    Add endpoint for courses

--- Spring/JPA-Database-communication/src/main/java/com/ltp/gradesubmission/web/CourseController.java
+++ Spring/JPA-Database-communication/src/main/java/com/ltp/gradesubmission/web/CourseController.java
@@ -1,12 +1,40 @@
 package com.ltp.gradesubmission.web;
 
+import com.ltp.gradesubmission.entity.Course;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 
 @RestController
 @RequestMapping("/course")
 public class CourseController {
 
+    @GetMapping("/all")
+    public ResponseEntity<List<Course>> getCourses() {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+    
+    @GetMapping("/{id}")
+    public ResponseEntity<Course> getCourse(@PathVariable final Long id) {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 
+    @PostMapping
+    public ResponseEntity<Course> saveCourse(@RequestBody final Course course) {
+        return new ResponseEntity<>(course, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<HttpStatus> deleteCourse(@PathVariable final Long id) {
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 }
