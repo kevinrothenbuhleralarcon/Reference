93612ecc2a568dec1fc46138dd25f025b55f97f6
KevinRothenbühler-Al <kevin.rothenbuhler-alarcon@softcom.pro>
2023-03-03 14:58:25 +0100

    Add a new input componant that handles errors

---
+++ Angular/Forms/src/app/reactive/input-with-errors/input-with-errors.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-input-with-errors',
+  templateUrl: './input-with-errors.component.html',
+  styleUrls: ['./input-with-errors.component.scss']
+})
+export class InputWithErrorsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- Angular/Forms/src/app/reactive/reactive.module.ts
+++ Angular/Forms/src/app/reactive/reactive.module.ts
@@ -3,11 +3,13 @@ import {CommonModule} from '@angular/common';
 import {ReactSignupComponent} from './react-signup/react-signup.component';
 import {ReactiveRoutingModule} from "./reactive-routing.module";
 import {ReactiveFormsModule} from "@angular/forms";
+import { InputWithErrorsComponent } from './input-with-errors/input-with-errors.component';
 
 
 @NgModule({
   declarations: [
-    ReactSignupComponent
+    ReactSignupComponent,
+    InputWithErrorsComponent
   ],
   imports: [
     CommonModule,
