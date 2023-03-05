6bc80a9b384cdd6936ec85bb2e2bab9c894858c9
Kevin Rothenbühler-Alarcon <kevin.rothenbuhler@gmail.com>
2023-03-05 13:20:31 +0100

    Remove editor config and start to pu the input with error on a shared folder

--- Angular/Forms/src/app/reactive/reactive.module.ts
+++ Angular/Forms/src/app/reactive/reactive.module.ts
@@ -3,7 +3,7 @@ import {CommonModule} from '@angular/common';
 import {ReactSignupComponent} from './react-signup/react-signup.component';
 import {ReactiveRoutingModule} from "./reactive-routing.module";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import { InputWithErrorsComponent } from './input-with-errors/input-with-errors.component';
+import {InputWithErrorsComponent} from '../shared-components/input-with-errors/input-with-errors.component';
 
 
 @NgModule({
